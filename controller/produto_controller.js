917b9d149a2a73898530ff402e0e3cfe2287f4ed
LeonardoRockstroh <leonardorockstroh@gmail.com>
2022-12-06 21:11:32 -0300

    validações back

--- controller/produto_controller.js
+++ controller/produto_controller.js
@@ -12,21 +12,31 @@ exports.listar = (req, res) => {
 
 exports.inserir = (req, res) => {
     const produtoRequest = req.body;
+
     if(produtoRequest && produtoRequest.nome && produtoRequest.preco) {
-    
-    
+        Cliente.find({ nome: produtoRequest.cliente }, (err, clienteEncontrado) => {
+
+            if(err  || clienteEncontrado.length == 0){
+                console.log('retornado 400')
+                return res.status(400).json({
+                    Erro:"Nome e/ou preco sao obrigatorios"
+                })
+            } else {
+                // Calcula valor produto
+                produtoRequest.valor = ( produtoRequest.qtd * produtoRequest.preco )
 
                 // Insere produto
-        const produtoNovo = new Produto(produtoRequest);
-        produtoNovo.save((err, produtoSalvo) => {
-            if(err) {
-                res.status(500).send(err);
-            }
-            else {
-                return res.status(201).json(produtoSalvo);
+                const produtoNovo = new Produto(produtoRequest);
+                produtoNovo.save((err, produtoSalvo) => {
+                    if(err) {
+                        res.status(500).send(err);
+                    }
+                    else {
+                        return res.status(201).json(produtoSalvo);
+                    }
+                })
             }
         })
-
     }
     else {
         console.log('retornado 400')
